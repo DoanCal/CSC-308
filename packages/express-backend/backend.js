999461c27a89bd37590b1d8f8048be13936c294d
DoanCal <doantcalpoly@gmail.com>
2026-01-22 00:17:06 -0800

    added POST method

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -40,6 +40,11 @@ const findUserByName = (name) => {
   );
 };
 
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
 
@@ -51,6 +56,12 @@ app.get("/users/:id", (req, res) => {
   } else {
     res.send(result);
   }
+});
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
 });
 
 app.get("/users", (req, res) => {
